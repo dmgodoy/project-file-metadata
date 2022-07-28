a6e778c5356569a3e9613d5f7da612ff0ad7133c
David M <davidmedinagodoy@gmail.com>
2022-07-28 18:04:29 +0000

    added endpoint to upload file

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer  = require('multer');
 
-var app = express();
+require('dotenv').config();
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+let app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,7 +15,9 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+});
 
 
 const port = process.env.PORT || 3000;
